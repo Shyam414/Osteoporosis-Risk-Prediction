43fa167d4dabab1e3d504a324b0804975e329c8c
Shyam414 <sundarreddy414@gmail.com>
2026-01-17 16:48:07 +0530

    commit

--- backend/utils/security.py
+++
@@ -1,7 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-def hash_password(password):
-    return generate_password_hash(password)
-
-def check_password(hash_pw, password):
-    return check_password_hash(hash_pw, password)
